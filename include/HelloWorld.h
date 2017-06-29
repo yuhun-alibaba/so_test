ac130447e7120f1b3eb94bd03fea744adbbea8bb
御魂 <wentao.swt@alibaba-inc.com>
2017-06-29 17:00:14 +0800

    for so test

---
+++ include/HelloWorld.h
@@ -0,0 +1,13 @@
+#ifndef HEELO_HELLOWORLD_H
+#define HEELO_HELLOWORLD_H
+
+#include <string>
+
+namespace hello {
+    class HelloWorld {
+    public:
+        static const void CallPrintLog();
+    };
+}
+
+#endif // HEELO_HELLOWORLD_H
---
+++ src/HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "../include/HelloWorld.h"
+#include <stdio.h>
+using namespace std;
+namespace hello {
+    void CallPrintLog() {
+        std::cout << "test, this is a helloworld test"; 
+    }
+}
+
+
